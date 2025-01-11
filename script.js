3cd20755ada93af9e745ea10629768234c99fe39
gioprogamertv123 <gioodabasi07@gmail.com>
2025-01-11 13:41:34 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector(".logo").onclick = function(){
+    window.location.href="file:///D:/Documents/Documents/programming%20for%20gio/Giorgi%20course%20programming/homewrok%20of%20desihn/index.html"
+}
